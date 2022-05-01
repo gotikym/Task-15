9da287b901cb73b5d98126af21b68e09212b6845
One <One@DESKTOP-MIB22JC>
2022-05-01 15:41:35 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите ваше имя");
+        string userName = Console.ReadLine();
+        Console.WriteLine("Введите символ");
+        string userSimbol = Console.ReadLine();
+        int OutputStringLength = 2 + Convert.ToInt32(userName.Length);
+        Console.WriteLine("\n");
+
+        for (int i = OutputStringLength; i > 0; i--)
+        {
+            Console.Write(userSimbol);
+        }
+        Console.WriteLine($"\n{userSimbol}{userName}{userSimbol}");
+
+        for (int i = OutputStringLength; i > 0; i--)
+        {
+            Console.Write(userSimbol);
+        }
+        Console.WriteLine("\n");
+    }
+}
